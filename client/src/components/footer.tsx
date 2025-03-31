b88b9ff389c74fbb9a961c989e795ce57072feb2
theEricDevelops <e-baker@users.noreply.github.com>
2025-03-31 09:46:51 -0400

    Create react client app

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,17 @@
+export const Footer = () => {
+  return (
+    <footer className="p-6 lg:fixed lg:bottom-0 lg:left-0">
+      <div>
+        visit{' '}
+        <a
+          href="https://ericdevelops.com/"
+          target="_blank"
+          //rel="noreferrer"
+          className="mt-4 inline-block underline"
+        >
+          ericdevelops
+        </a>
+      </div>
+    </footer>
+  );
+};
